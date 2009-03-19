70efe9fd1c15a7ea69e78af25141f9a6de9ce246
Daniel Morris <daniel.john.morris@googlemail.com>
2009-03-19 09:24:20 +0000

    Cleaned up section examples

--- game.js
+++ game.js
@@ -1,17 +1,12 @@
 function Game(turns, players){ 
-	//this.constructor.current_turn++;
+	//this.constructor.awesomeness++;
 
 	// ************************************************************************ 
 	// PRIVATE VARIABLES AND FUNCTIONS 
 	// ONLY PRIVELEGED METHODS MAY VIEW/EDIT/INVOKE 
 	// *********************************************************************** 
 
-    
-	//var alive=true, age=1;
-	//var maxAge=70+Math.round(Math.random()*15)+Math.round(Math.random()*15);
 	//var myName=n?n:"John Doe";
-	//var weight=1;
-
 	//function makeOlder(){ return alive = (++age <= maxAge) } 
 
 	// ************************************************************************ 
@@ -61,22 +56,7 @@ function Game(turns, players){
 	//this.toString=this.getName=function(){ return myName } 
 
 	//this.eat=function(){ 
-	//	if (makeOlder()){ 
-	//		this.dirtFactor++;
-	//		return weight*=3;
-	//	} else alert(myName+" can't eat, he's dead!");
 	//} 
-	//this.exercise=function(){ 
-	//	if (makeOlder()){ 
-	//		this.dirtFactor++;
-	//		return weight/=2;
-	//	} else alert(myName+" can't exercise, he's dead!");
-	//} 
-	//this.weigh=function(){ return weight } 
-	//this.getRace=function(){ return race } 
-	//this.getAge=function(){ return age } 
-	//this.muchTimePasses=function(){ age+=50; this.dirtFactor=10; } 
-
 
 	// ************************************************************************ 
 	// PUBLIC PROPERTIES -- ANYONE MAY READ/WRITE 
@@ -87,8 +67,7 @@ function Game(turns, players){
     this.players = players;
     this.tick = null;
 
-	//this.clothing="nothing/naked";
-	//this.dirtFactor=0;
+	//this.attribute = "some-default";
 } 
 
 
@@ -96,22 +75,19 @@ function Game(turns, players){
 // PUBLIC METHODS -- ANYONE MAY READ/WRITE 
 // ************************************************************************ 
 
-//Person.prototype.beCool = function(){ this.clothing="khakis and black shirt" } 
-//Person.prototype.shower = function(){ this.dirtFactor=2 } 
-//Person.prototype.showLegs = function(){ alert(this+" has "+this.legs+" legs") } 
-//Person.prototype.amputate = function(){ this.legs-- } 
+//Thing.prototype.beAwesome = function(){ this.attribute = "AWESOME!" } 
 
 
 // ************************************************************************ 
 // PROTOTYOPE PROERTIES -- ANYONE MAY READ/WRITE (but may be overridden) 
 // ************************************************************************ 
 
-//Person.prototype.legs=2;
+//Thing.prototype.legs = 2;
 
 
 // ************************************************************************ 
 // STATIC PROPERTIES -- ANYONE MAY READ/WRITE 
 // ************************************************************************ 
 
-//Game.current_turn = 0;
+//Thing.awesomeness = 0;
 
